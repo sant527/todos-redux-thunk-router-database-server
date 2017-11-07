b9ccc595f976d6dbef6f1f4d8a6fff0793e7de49
simha Rupa Das <sant527@gmail.com>
2017-11-08 00:02:00 +0530

    Lesson 4: Redux - Refactoring the Entry Point-1060.mp4

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import todoApp from './reducers'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle'
+import configureStore from './configureStore';
+import Root from './components/Root'
 
-
-const persistedState = loadState()
-
-
-const store = createStore(
-	todoApp,
-	persistedState
-);
-console.log(store.getState());
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  })
-}, 1000))
+const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ <Root store={store} />,
   document.getElementById('root')
 )
