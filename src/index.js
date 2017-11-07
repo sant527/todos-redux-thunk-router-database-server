14d25f674a2cd736000a440f7ae4cfdd2213dc1a
simha Rupa Das <sant527@gmail.com>
2017-11-07 19:05:17 +0530

    Lesson 2: Redux - Supplying the Initial State-1056

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
+import todoApp from './reducers'
 
-const store = createStore(reducer)
+
+const persistedState = {
+	todos: [{
+		id: '0',
+		text: 'Welcome back!',
+		completed: false,
+	}],
+	visibilityFilter: undefined
+}
+
+
+const store = createStore(
+	todoApp,
+	persistedState
+);
+console.log(store.getState());
 
 render(
   <Provider store={store}>
