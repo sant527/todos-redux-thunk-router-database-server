1c833b2332a8648e7e1b7e7e9f8fd162cec6aeb6
simha Rupa Das <sant527@gmail.com>
2017-11-10 16:51:21 +0530

    Lesson 10: Redux - Colocating Selectors with Reducers-1071.mp4

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,24 +1,13 @@
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
-import TodoList from '../components/TodoList'
 import { withRouter } from 'react-router-dom';
+import { toggleTodo } from '../actions'
+import { getVisibleTodos } from '../reducers'
+import TodoList from '../components/TodoList'
 
-const getVisibleTodos = (todos, filter) => {
-  console.log('getVisibleTodos filter: ' + filter)
-  switch (filter) {
-    case 'all':
-      return todos
-    case 'completed':
-      return todos.filter(t => t.completed)
-    case 'active':
-      return todos.filter(t => !t.completed)
-    default:
-      throw new Error('Unknown filter: ' + filter)
-  }
-}
+
 
 const mapStateToProps = (state, ownProps) => ({
-  todos: getVisibleTodos(state.todos, ownProps.match.params.filter || 'all')
+  todos: getVisibleTodos(state, ownProps.match.params.filter || 'all')
 })
 
 /*const mapDispatchToProps = (dispatch) => ({
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -21,3 +21,21 @@ const todos = (state = [], action) => {
 }
 
 export default todos
+
+//The convention we follow is simple. The default export is always the reducer function, but any named export starting with 'get' is a function that prepares the data to be displayed by the UI. We usually call these functions selectors because they select something from the current state.
+
+//In the reducers, the state argument corresponds to the state of this particular reducer, so we will follow the same convention for selectors. The state argument corresponds to the state of the exported reducer in this file.
+
+export const getVisibleTodos = (todos, filter) => {
+  console.log('getVisibleTodos filter: ' + filter)
+  switch (filter) {
+    case 'all':
+      return todos
+    case 'completed':
+      return todos.filter(t => t.completed)
+    case 'active':
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
+}
