fedfb3f397cde8db6709a91db68a265d4019622e
simha Rupa Das <sant527@gmail.com>
2017-11-10 17:36:59 +0530

    Lesson 11: Redux - Normalizing the State Shape-1072.mp4

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,40 +1,51 @@
-const todos = (state = [], action) => {
+import { combineReducers } from 'redux'
+import todo from './todo';
+
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    //If you forget a break/return then script will run from the case where criteria is met, and will run the case after that regardless if criteria was met. 
+    case 'ADD_TODO':
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action),
+      };
+    default:
+      return state;
+  }
+};
+
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id) 
-          ? {...todo, completed: !todo.completed}
-          : todo
-      )
+      return [...state, action.id];
     default:
-      return state
+      return state;
   }
-}
+};
+
+
+const todos = combineReducers({
+  byId,
+  allIds,
+});
+
 
 export default todos
 
-//The convention we follow is simple. The default export is always the reducer function, but any named export starting with 'get' is a function that prepares the data to be displayed by the UI. We usually call these functions selectors because they select something from the current state.
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
 
-//In the reducers, the state argument corresponds to the state of this particular reducer, so we will follow the same convention for selectors. The state argument corresponds to the state of the exported reducer in this file.
-
-export const getVisibleTodos = (todos, filter) => {
+export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
   console.log('getVisibleTodos filter: ' + filter)
   switch (filter) {
     case 'all':
-      return todos
+      return allTodos
     case 'completed':
-      return todos.filter(t => t.completed)
+      return allTodos.filter(t => t.completed)
     case 'active':
-      return todos.filter(t => !t.completed)
+      return allTodos.filter(t => !t.completed)
     default:
       throw new Error('Unknown filter: ' + filter)
   }
