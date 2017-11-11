c84c74a9ed93e151a2ea36424abf89de0e6d6b3d
santhosh <simharupa.rns@gmail.com>
2017-11-11 11:36:43 +0530

    Lesson 18 & 19: Applying_Redux_Middleware & Updating_the_State_with_the_Fetched

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,48 +1,19 @@
-import { createStore } from 'redux'
-import todoApp from './reducers'
-
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-  		return rawDispatch
-	}
-  return (action) => {
-    console.group(action.type);
-	console.log('%c prev state', 'color: gray', store.getState());
-	console.log('%c action', 'color: blue', action);
-	const returnValue = rawDispatch(action);
-	console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  }
-}
-
-const addPromiseSupportToDispatch = (store) => {
-	const rawDispatch = store.dispatch;
-	return (action) => {
-		if(typeof action.then === 'function'){
-			return action.then(rawDispatch);
-		}
-		return rawDispatch(action);
-	}
-}
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
+import todoApp from './reducers';
 
 const configureStore = () => {
+  const middlewares = [promise];
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+    // Note: you can supply options to `createLogger()`
+  }
 
-	const store = createStore(
-		todoApp
-	);
-	
-	if (process.env.NODE_ENV !== 'production') {
-      store.dispatch = addLoggingToDispatch(store);
-    }
-
-    store.dispatch = addPromiseSupportToDispatch(store);
-
-	return store;
-}
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  );
+};
 
 export default configureStore;
-
-
-
